652c473b678be13dfc947db28b58fd8a541b5f82
kimHJ <khj64076407@gmail.com>
2020-01-13 09:48:50 +0900

    main

---
+++ js/map.js
@@ -0,0 +1,17 @@
+function initialize() {
+  var myLatlng = new google.maps.LatLng(36.357993, 127.329648);
+  var mapOptions = {
+    zoom: 17,
+    center: myLatlng
+  };
+  var map = new google.maps.Map(
+    document.getElementById("map_canvas"),
+    mapOptions
+  );
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "MY HOUSE"
+  });
+}
+google.maps.event.addDomListener(window, "load", initialize);
