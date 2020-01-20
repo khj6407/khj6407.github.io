002e0801df3c64f8d83fa831804182a750ccb1a9
kimHJ <khj64076407@gmail.com>
2020-01-20 12:58:56 +0900

    map

--- js/map.js
+++ js/map.js
@@ -1,17 +1,25 @@
-function initialize() {
-  var myLatlng = new google.maps.LatLng(36.357993, 127.329648);
-  var mapOptions = {
-    zoom: 17,
-    center: myLatlng
-  };
-  var map = new google.maps.Map(
-    document.getElementById("map_canvas"),
-    mapOptions
-  );
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: "MY HOUSE"
-  });
-}
-google.maps.event.addDomListener(window, "load", initialize);
+// function initialize() {
+//   var myLatlng = new google.maps.LatLng(36.357993, 127.329648);
+//   var mapOptions = {
+//     zoom: 17,
+//     center: myLatlng
+//   };
+//   var map = new google.maps.Map(
+//     document.getElementById("map_canvas"),
+//     mapOptions
+//   );
+//   var marker = new google.maps.Marker({
+//     position: myLatlng,
+//     map: map,
+//     title: "MY HOUSE"
+//   });
+// }
+// google.maps.event.addDomListener(window, "load", initialize);
+
+var container = document.getElementById("map");
+var options = {
+  center: new kakao.maps.LatLng(36.357993, 127.329648),
+  level: 3
+};
+
+var map = new kakao.maps.Map(container, options);
